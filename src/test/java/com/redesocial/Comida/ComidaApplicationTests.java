7d9563fae75e988597726b663095c975db4fe6b8
Tiago-Lucas <lucastiago45@gmail.com>
2021-02-23 14:56:15 -0300

    Projeto criado

---
+++ src/test/java/com/redesocial/Comida/ComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redesocial.Comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
