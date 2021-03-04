3716b8e94c9799b441e6d1ccdc15a1373985f50e
Tiago-Lucas <lucastiago45@gmail.com>
2021-03-04 16:06:34 -0300

    Adicionando Post

--- src/main/java/com/redesocial/Comida/service/UserService.java
+++ src/main/java/com/redesocial/Comida/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redesocial.Comida.domain.User;
+import com.redesocial.Comida.dto.UserDTO;
 import com.redesocial.Comida.repository.UserRepository;
 import com.redesocial.Comida.service.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
