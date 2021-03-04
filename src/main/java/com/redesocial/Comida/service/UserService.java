2bfe07979b54fc374e7e249420de7c40abe31bab
Tiago-Lucas <lucastiago45@gmail.com>
2021-03-04 15:36:57 -0300

    Obtendo um usuario por id

--- src/main/java/com/redesocial/Comida/service/UserService.java
+++ src/main/java/com/redesocial/Comida/service/UserService.java
@@ -1,12 +1,14 @@
 package com.redesocial.Comida.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.redesocial.Comida.domain.User;
 import com.redesocial.Comida.repository.UserRepository;
+import com.redesocial.Comida.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 }
