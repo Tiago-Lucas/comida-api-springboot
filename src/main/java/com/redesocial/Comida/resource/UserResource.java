8bb00cc46a8738edd204ac73f3ede2fb53c24134
Tiago-Lucas <lucastiago45@gmail.com>
2021-03-07 17:44:50 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/redesocial/Comida/resource/UserResource.java
+++ src/main/java/com/redesocial/Comida/resource/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.redesocial.Comida.domain.Post;
 import com.redesocial.Comida.domain.User;
 import com.redesocial.Comida.dto.UserDTO;
 import com.redesocial.Comida.service.UserService;
@@ -57,6 +58,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
